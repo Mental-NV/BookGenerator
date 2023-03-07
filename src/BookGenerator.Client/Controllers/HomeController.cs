6b39379c2e986537bb3b767f7f100c327da11920
Ruslan Galiev <ruslangaliev@microsoft.com>
2023-03-07 02:41:55 +0200

    Add project files.

---
+++ src/BookGenerator.Client/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using BookGenerator.Client.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BookGenerator.Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public IActionResult Generate(string input)
+        {
+            return Content($"Ok: {input}");
+        }
+    }
+}
