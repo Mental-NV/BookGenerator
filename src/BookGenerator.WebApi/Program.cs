3ca0dfd096184a5d8a975d94634fb5ee4014f54d
Guzel Ganieva <guzel95@list.ru>
2023-03-20 23:32:48 +0300

    changed framework version to .net 6

--- src/BookGenerator.WebApi/Program.cs
+++ src/BookGenerator.WebApi/Program.cs
@@ -2,7 +2,6 @@ using BookGenerator.Application;
 using BookGenerator.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
 builder.Services
     .AddApplication()
@@ -20,13 +19,11 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
