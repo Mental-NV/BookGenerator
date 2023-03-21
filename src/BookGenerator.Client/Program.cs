464c1c9a8c94afb32d4e5bf59d0f5a3e6bf9f0b9
Mental-NV <ruslan.galiev@outlook.com>
2023-03-22 01:51:46 +0200

    Added production settings

--- src/BookGenerator.Client/Program.cs
+++ src/BookGenerator.Client/Program.cs
@@ -33,13 +33,7 @@ builder.Services
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+app.UseDeveloperExceptionPage();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
--- src/BookGenerator.WebApi/Program.cs
+++ src/BookGenerator.WebApi/Program.cs
@@ -19,11 +19,9 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
