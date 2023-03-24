26648ea10693ab093456380a55d2fda3aa725b4d
Mental-NV <ruslan.galiev@outlook.com>
2023-03-24 03:47:10 +0200

    Added unit tests

--- src/BookGenerator.Application/Books/Commands/CreateBook/CreateBookCommandValidator.cs
+++ src/BookGenerator.Application/Books/Commands/CreateBook/CreateBookCommandValidator.cs
@@ -11,9 +11,13 @@ public class CreateBookCommandValidator : AbstractValidator<CreateBookCommand>
             .Cascade(CascadeMode.Stop)
             .NotEmpty().WithMessage("Title cannot be empty.")
             .Length(1, 100).WithMessage("Title must be between 1 and 100 characters long.")
-            .Matches(@"^[\p{L}\p{Nd} ,'\-]+$").WithMessage("Title can only contain letters, digits, spaces, commas, hyphens, and apostrophes.")
-            .Matches(@"^[\p{L}\p{Nd}].*[\p{L}\p{Nd}]$").WithMessage("Title must start and end with a letter or digit.")
-            .Matches(@"^[^,\-']*(,[^,\-']*[\p{L}\p{Nd}][^,\-']*)*$").WithMessage("Title can only contain consecutive commas if they are separated by letters or digits.")
-            .Matches(@"^[^-',]+(-[^-',]+)*$").WithMessage("Title can only contain consecutive hyphens if they are separated by letters or digits.");
+            .Matches(new Regex(@"^[\p{L}\p{N}]+[\p{L}\p{N} ,'-]*[\p{L}\p{N}]+$")).WithMessage("Title can only contain letters, digits, spaces, commas, hyphens, and apostrophes, and must start and end with a letter or digit.")
+            .Must(NotHaveConsecutiveSpacesOrPunctuation).WithMessage("Title cannot have consecutive spaces or punctuation marks.");
+    }
+
+    private bool NotHaveConsecutiveSpacesOrPunctuation(string title)
+    {
+        var consecutiveSpacesOrPunctuation = new Regex(@"(\s\s)|([,'-]{2})");
+        return !consecutiveSpacesOrPunctuation.IsMatch(title);
     }
 }
---
+++ tests/BookGenerator.Application.UnitTests/Books/Commands/CreateBook/CreateBookCommandValidatorTests.cs
@@ -0,0 +1,93 @@
+﻿using BookGenerator.Application.Books.Commands.CreateBook;
+using BookGenerator.Domain.Core;
+using FluentValidation;
+using FluentValidation.TestHelper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Security.Cryptography.X509Certificates;
+
+namespace BookGenerator.Application.UnitTests.Books.Commands.CreateBook
+{
+    [TestClass]
+    public class CreateBookCommandValidatorTests
+    {
+        private CreateBookCommandValidator validator;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            validator = new CreateBookCommandValidator();
+        }
+
+        [TestMethod]
+        public void TitleIsEmpty_ShouldHaveError()
+        {
+            var model = new CreateBookCommand(string.Empty);
+            var result = validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.BookTitle);
+        }
+
+        [TestMethod]
+        public void TitleIsTooShort_ShouldHaveError()
+        {
+            var model = new CreateBookCommand("a");
+            var result = validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.BookTitle);
+        }
+
+        [TestMethod]
+        public void TitleIsTooLong_ShouldHaveError()
+        {
+            var model = new CreateBookCommand(new string('A', 101));
+            var result = validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.BookTitle);
+        }
+
+        [TestMethod]
+        public void TitleContainsInvalidCharacters_ShouldHaveError()
+        {
+            var model = new CreateBookCommand("Title with invalid character: @");
+            var result = validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.BookTitle);
+        }
+
+        [TestMethod]
+        public void TitleDoesNotStartWithLetterOrDigit_ShouldHaveError()
+        {
+            var model = new CreateBookCommand("-Invalid title start");
+            var result = validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.BookTitle);
+        }
+
+        [TestMethod]
+        public void TitleDoesNotEndWithLetterOrDigit_ShouldHaveError()
+        {
+            var model = new CreateBookCommand("Invalid title end-");
+            var result = validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.BookTitle);
+        }
+
+        [TestMethod]
+        public void TitleHasConsecutiveCommas_ShouldHaveError()
+        {
+            var model = new CreateBookCommand("Title with consecutive,, commas");
+            var result = validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.BookTitle);
+        }
+
+        [TestMethod]
+        public void TitleHasConsecutiveHyphens_ShouldHaveError()
+        {
+            var model = new CreateBookCommand("Title with consecutive-- hyphens");
+            var result = validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.BookTitle);
+        }
+
+        [TestMethod]
+        public void TitleIsValid_ShouldNotHaveError()
+        {
+            var model = new CreateBookCommand("A valid title - Пример допустимого заглавия, 有效的标题");
+            var result = validator.TestValidate(model);
+            result.ShouldNotHaveValidationErrorFor(x => x.BookTitle);
+        }
+    }
+}
