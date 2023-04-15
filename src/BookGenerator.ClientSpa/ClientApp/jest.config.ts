70826fac3aebfd18f9cedca367060d1773333dc2
Mental-NV <ruslan.galiev@outlook.com>
2023-04-15 17:50:41 +0300

    Configured Jest to use TypeScript's configuration file

--- src/BookGenerator.ClientSpa/ClientApp/jest.config.ts
+++ src/BookGenerator.ClientSpa/ClientApp/jest.config.ts
@@ -1,7 +1,11 @@
-﻿module.exports = {
+﻿import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
     preset: 'ts-jest',
     testEnvironment: 'jest-environment-jsdom',
     transform: {
         '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
-    }
+    },
 };
+
+export default config;
