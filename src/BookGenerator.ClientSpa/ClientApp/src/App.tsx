43c797ccfc5c51b7388f1257e72dcc7216e4bab0
Mental-NV <ruslan.galiev@outlook.com>
2023-04-18 04:37:08 +0300

    Load pages on demand

--- src/BookGenerator.ClientSpa/ClientApp/src/App.tsx
+++ src/BookGenerator.ClientSpa/ClientApp/src/App.tsx
@@ -1,18 +1,21 @@
-﻿import React from 'react'
+﻿import React, { lazy, Suspense } from 'react'
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Route, Switch } from 'react-router-dom';
-import StatusPage from './pages/StatusPage';
-import HomePage from './pages/HomePage';
-import DownloadPage from './pages/DownloadPage';
+
+const StatusPage = lazy(() => import('./pages/StatusPage'));
+const HomePage = lazy(() => import('./pages/HomePage'));
+const DownloadPage = lazy(() => import('./pages/DownloadPage'));
 
 const App: React.FC = () => {
     return (
         <Router>
-            <Switch>
-                <Route exact path="/" component={HomePage} />
-                <Route path="/status/:bookId" component={StatusPage} />
-                <Route path="/download/:bookId" component={DownloadPage} />
-            </Switch>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Switch>
+                    <Route exact path="/" component={HomePage} />
+                    <Route path="/status/:bookId" component={StatusPage} />
+                    <Route path="/download/:bookId" component={DownloadPage} />
+                </Switch>
+            </Suspense>
         </Router>
     );
 }
