90c06165cc40b015a75273bc764e3e56fec9833b
Mental-NV <ruslan.galiev@outlook.com>
2023-04-20 03:25:47 +0300

    Added css for BookForm

--- src/BookGenerator.ClientSpa/ClientApp/src/components/BookForm.tsx
+++ src/BookGenerator.ClientSpa/ClientApp/src/components/BookForm.tsx
@@ -1,4 +1,5 @@
 ﻿import React, { useState } from 'react'
+import styles from './BookForm.module.css';
 
 interface BookFormProps {
     onGenerate: (title: string) => void;
@@ -19,12 +20,12 @@ const BookForm: React.FC<BookFormProps> = ({ onGenerate }) => {
     };
 
     return (
-        <div>
+        <div className={styles.container}>
             <h1>Book Generator</h1>
             <form onSubmit={handleSubmit} role="form">
                 <label htmlFor="book-title">Title</label>
-                <input type="text" id="bookTitleInput" data-testid="bookTitleInput" value={title} onChange={handleChange} disabled={isFormDisabled} />
-                <button type="submit" id="generateButton" data-testid="generateButton" disabled={isFormDisabled}>Generate</button>
+                <input type="text" id="bookTitleInput" data-testid="bookTitleInput" value={title} onChange={handleChange} disabled={isFormDisabled} className={styles.input} />
+                <button type="submit" id="generateButton" data-testid="generateButton" disabled={isFormDisabled} className={styles.button}>Generate</button>
             </form>
         </div>
     );
