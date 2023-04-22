c90f1ddfcf2d0a2632d261670b07cb53541878be
Mental-NV <ruslan.galiev@outlook.com>
2023-04-23 02:51:44 +0300

    Added Tailwind CSS

--- src/BookGenerator.ClientSpa/ClientApp/src/components/BookForm.tsx
+++ src/BookGenerator.ClientSpa/ClientApp/src/components/BookForm.tsx
@@ -19,12 +19,16 @@ const BookForm: React.FC<BookFormProps> = ({ onGenerate }) => {
     };
 
     return (
-        <div>
-            <h1>Book Generator</h1>
-            <form onSubmit={handleSubmit} role="form">
-                <label htmlFor="book-title">Title</label>
-                <input type="text" id="bookTitleInput" data-testid="bookTitleInput" value={title} onChange={handleChange} disabled={isFormDisabled} />
-                <button type="submit" id="generateButton" data-testid="generateButton" disabled={isFormDisabled}>Generate</button>
+        <div className="container h-screen mx-auto py-8">
+            <h1 className="text-2xl font-bold text-center mb-6 mt-8">Book Generator</h1>
+            <form className="w-full max-w-sm mx-auto bg-white p-8 rounded-md shadow-md" onSubmit={handleSubmit} role="form">
+                <label className="hidden text-gray-700 text-sm font-bold mr-2" htmlFor="book-title">Title</label>
+                <input className="px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                    type="text" id="bookTitleInput" data-testid="bookTitleInput" value={title}
+                    placeholder="Enter a book title"
+                    onChange={handleChange} disabled={isFormDisabled} />
+                <button className="bg-indigo-500 text-white text-sm font-bold py-2 px-4 rounded-md ml-5 hover:bg-indigo-600 transition duration-300"
+                    type="submit" id="generateButton" data-testid="generateButton" disabled={isFormDisabled}>Generate</button>
             </form>
         </div>
     );
