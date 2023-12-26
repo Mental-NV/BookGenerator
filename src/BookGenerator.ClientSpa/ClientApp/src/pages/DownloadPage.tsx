64727c9e2ed0355a88bb616b55f738765d35395c
MentalNV <ruslan.galiev@outlook.com>
2023-12-26 20:52:23 +0200

    Display an error when downloading a book failed

--- src/BookGenerator.ClientSpa/ClientApp/src/pages/DownloadPage.tsx
+++ src/BookGenerator.ClientSpa/ClientApp/src/pages/DownloadPage.tsx
@@ -10,15 +10,37 @@ interface RouteParams {
 const DownloadPage: React.FC = () => {
     const { bookId } = useParams<RouteParams>();
     const [bookFile, setBookFile] = useState<BookFile | null>(null);
+    const [error, setError] = useState<string | null>(null);
 
     useEffect(() => {
         const fetchBookFile = async () => {
-            const bookFileData = await getBookFile(bookId);
-            setBookFile(bookFileData);
+            try {
+                const bookFileData = await getBookFile(bookId);
+                setBookFile(bookFileData);
+                setError(null); // reset error on successfult fetch
+            } catch (err) {
+                if (err instanceof Error) {
+                    setError(err.message);
+                }
+            }
+
         };
 
         fetchBookFile();
     }, [bookId]);
+
+    if (error) {
+        return (
+            <div className="flex flex-col min-h-screen justify-center items-center">
+                <div className="w-full max-w-2xl h-52 flex flex-col">
+                    <h1 className="text-center text-slate-900 font-bold text-3xl mb-4">Download Page</h1>
+                    <p className="text-center">
+                        An error occured: {error}
+                    </p>
+                </div>
+            </div>
+        );
+    }
 
     if (!bookFile) {
         return (
